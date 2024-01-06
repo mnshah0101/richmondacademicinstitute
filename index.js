c8e81dbec937c1f9b2b625f913e6c69082b53eb8
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-01-05 23:56:06 -0500

    latest

--- index.js
+++ index.js
@@ -8,12 +8,44 @@ app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // Route for rendering the ejs view
-app.get('/', function(req, res) {
-  res.render('home', { title: 'Hey', message: 'Hello there!' });
-});
+
 
 // setting static files location
 app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get('/about', function(req, res) {
+  res.render('about');
+}
+);
+app.get('/reviews', function(req, res) {
+  res.render('review');
+}
+);
+
+app.get('/contact', function(req, res) {
+  res.render('contact');
+}
+);
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+
+//if any other route is requested, render 404
+app.get('*', function(req, res) {
+  res.render('404');
+}
+);
+
+
+
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('404');
+});
+
+
 
 // Set the server to listen on port 3000
 app.listen(3000, function () {
